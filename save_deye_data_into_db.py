9e5242aa66f7d42f59d3042a2af0147cc1dd638b
Markus Voge <sgrubsmyon@users.noreply.github.com>
2023-08-10 23:40:33 +0200

    Basic sampling loop finished

--- save_deye_data_into_db.py
+++ save_deye_data_into_db.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from datetime import datetime
+import time
 import numpy as np
 
 start_point = {
@@ -22,20 +23,34 @@ for group in start_point:
     sampling_points[group] = np.arange(
         minute_startpoint, 60, minute_interval)
 
-now = datetime.now()
-now_minute = now.minute + now.second / 60 + now.microsecond * 1e-6 / 60
-print("now:", now_minute)
+def sample():
+    now = datetime.now()
+    now_minute = now.minute + now.second / 60 + now.microsecond * 1e-6 / 60
+    # print(f"Now: {now.minute}:{now.second + now.microsecond * 1e-6}")
 
-closest_sampling_point = {}
-for group in sampling_points:
-    delta_ts = sampling_points[group] - now_minute
-    # Remove sampling points of the past (negative time difference delta_t)
-    delta_ts = delta_ts[delta_ts > 0]
-    delta_t = np.min(delta_ts)
-    closest_sampling_point[group] = delta_t
-next_sampling_group = min(closest_sampling_point, key=closest_sampling_point.get)
-next_sampling_delta_t = closest_sampling_point[next_sampling_group]
-next_sampling_minute = now_minute + next_sampling_delta_t
-next_sampling_full_minute = int(np.floor(next_sampling_minute))
-next_sampling_full_second = (next_sampling_minute - next_sampling_full_minute) * 60
-print(f"next sampling: group {next_sampling_group} at {next_sampling_full_minute}:{next_sampling_full_second}")
+    closest_sampling_point = {}
+    for group in sampling_points:
+        delta_ts = sampling_points[group] - now_minute
+        # Remove sampling points of the past (negative time difference delta_t)
+        delta_ts = delta_ts[delta_ts > 0]
+        # Find next upcoming sampling point (closest in time in the future)
+        delta_t = np.min(delta_ts)
+        closest_sampling_point[group] = delta_t
+    # Find which metric group has the smallest delta_t
+    next_sampling_group = min(closest_sampling_point, key=closest_sampling_point.get)
+    # Minutes until next sampling:
+    next_sampling_delta_t = closest_sampling_point[next_sampling_group]
+
+    # Wait until the next sampling point is due
+    # time.sleep() takes seconds as parameter
+    time.sleep(next_sampling_delta_t * 60)
+
+    now = datetime.now()
+    now_minute = now.minute + now.second / 60 + now.microsecond * 1e-6 / 60
+    print(f"{now.minute}:{now.second + now.microsecond * 1e-6}: Sampling '{next_sampling_group}'")
+
+    # Repeat the same process:
+    sample()
+
+# Start the infinite sampling loop:
+sample()
