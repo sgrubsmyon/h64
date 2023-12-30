88981561d1427c5966785d9d9af3c2a59994fd7b
Markus Voge <sgrubsmyon@users.noreply.github.com>
2023-12-30 02:01:19 +0100

    Add H64 logo

--- frontend/h64/src/main.ts
+++ frontend/h64/src/main.ts
@@ -1,17 +1,14 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
+import h64Logo from '/h64-logo.svg'
+import h64LogoDark from '/h64-logo-for-dark.svg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    <a href="/">
+      <img src="${h64LogoDark}" class="logo" alt="H64 logo" />
     </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>H64</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
