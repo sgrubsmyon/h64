6956786e0d14a575e2203cf7a8a55c23138e0b56
Markus Voge <sgrubsmyon@users.noreply.github.com>
2024-07-23 23:06:08 +0200

    Better debug log

--- weather_station/bun_ws_server/weather_station_websocket_server.js
+++ weather_station/bun_ws_server/weather_station_websocket_server.js
@@ -92,8 +92,7 @@ const server = Bun.serve({
         console.log("Message values:", msg.values);
         // Update the current values with new ones:
         for (const [key, value] of Object.entries(msg.values)) {
-          console.log("Message key:", key);
-          console.log("Message value:", value);
+          console.log(key, value);
           // if the value is valid, adopt it as new value
           if (value !== null && value !== undefined) {
             CURR_VALUES[key] = value;
