b0af05c5d5f2924303a9f1830a66fc9531f96db2
Markus Voge <sgrubsmyon@users.noreply.github.com>
2024-07-29 23:43:17 +0200

    Margin bottom for value display card

--- frontend/h64-quasar/src/components/ValueDisplay.vue
+++ frontend/h64-quasar/src/components/ValueDisplay.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-card dark bordered class="my-card q-mr-md">
+  <q-card dark bordered class="my-card q-mr-md q-mb-md">
     <q-card-section>
       <div class="text-h6">{{ label }}</div>
     </q-card-section>
@@ -7,20 +7,26 @@
     <q-separator dark inset />
 
     <q-card-section>
-      <div class="text-h1">{{ value }}</div>
+      <div class="text-h2">{{ value }}</div>
     </q-card-section>
   </q-card>
 </template>
 
+<style scoped>
+  .my-card {
+    width: 100%;
+    max-width: 350px;
+  }
+</style>
+
 <script setup lang="ts">
+  interface Props {
+    label: string;
+    value: string;
+  };
 
-interface Props {
-  label: string;
-  value: string;
-};
-
-withDefaults(defineProps<Props>(), {
-  label: '',
-  value: '-'
-});
+  withDefaults(defineProps<Props>(), {
+    label: '',
+    value: '-'
+  });
 </script>
