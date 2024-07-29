ef11fce21a1e84218ab216177cd51dfd63e17aba
Markus Voge <sgrubsmyon@users.noreply.github.com>
2024-07-29 23:43:49 +0200

    Show more data

--- frontend/h64-quasar/src/pages/WeatherLive.vue
+++ frontend/h64-quasar/src/pages/WeatherLive.vue
@@ -3,6 +3,17 @@ import { ref } from 'vue';
 import ValueDisplay from 'components/ValueDisplay.vue';
 
 const values = ref({
+  time: '',
+  model: '',
+  id: NaN,
+  channel: NaN,
+  battery_ok: NaN,
+  sensor_type: NaN,
+  wind_max_m_s: NaN,
+  wind_avg_m_s: NaN,
+  wind_dir_deg: NaN,
+  rain_mm: NaN,
+  mic: '',
   temperature_C: NaN,
   humidity: NaN,
 });
@@ -17,17 +28,15 @@ socket.onmessage = function (e) {
 <template>
   <q-page padding>
     <h6>Current weather values:</h6>
+    <p>Letzte Aktualisierung: {{ values.time }}</p>
 
     <div class="row">
-      <value-display id="temperature" label="Temperature (outside)" :value="`${values.temperature_C} °C`" />
-      <value-display id="temperature" label="Humidity (outside)" :value="`${values.humidity}%`" />
+      <value-display label="Temperature (outside)" :value="`${values.temperature_C} °C`" />
+      <value-display label="Humidity (outside)" :value="`${values.humidity}%`" />
+      <value-display label="Wind Average" :value="`${values.wind_avg_m_s} m/s`" />
+      <value-display label="Wind Gust" :value="`${values.wind_max_m_s} m/s`" />
+      <value-display label="Wind Direction" :value="`${values.wind_dir_deg}°`" />
+      <value-display label="Total Rainfall since last reset" :value="`${values.rain_mm} Liter`" />
     </div>
   </q-page>
 </template>
-
-<style>
-.my-card {
-  width: 100%;
-  max-width: 350px;
-}
-</style>
