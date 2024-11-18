7d348b9377d7001c88b3ffba8cf8854681401a8b
Markus Voge <sgrubsmyon@users.noreply.github.com>
2024-11-19 00:19:37 +0100

    Add SQL code for creating heat pump table

---
+++ heat_pump/mqtt-server-raspi/create_heat_pump_metrics_tables.sql
@@ -0,0 +1,10 @@
+-- Create tables inside PostgreSQL TimescaleDB database `weather`
+-- for holding data read via RTL-SDR antenna from a Bresser 5-in-1 weather station
+
+-- Table is written to upon every pulse of the heat pump power meter (one pulse every 0.001 kWh)
+CREATE TABLE heat_pump_metrics_electric_power_pulse (
+   time TIMESTAMPTZ NOT NULL, -- time in the following format: "2024-11-18 20:28:16"
+);
+
+-- Convert tables to TimescaleDB hypertables:
+SELECT create_hypertable('heat_pump_metrics_electric_power_pulse', 'time');
