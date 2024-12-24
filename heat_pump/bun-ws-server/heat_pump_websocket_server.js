3c09b46004d0d81bcb146b8c276f91ced92f4476
Markus Voge <sgrubsmyon@users.noreply.github.com>
2024-12-25 00:28:51 +0100

    Log pool on disconnect

--- heat_pump/bun-ws-server/heat_pump_websocket_server.js
+++ heat_pump/bun-ws-server/heat_pump_websocket_server.js
@@ -191,6 +191,7 @@ const server = Bun.serve({
     close(ws) {
       N_CONN_CLIENTS--;
       POOL[ws.data.socket_id] = null;
+      console.log("POOL:", POOL);
       if (DEBUG) {
         console.log(`Disconnected, now ${N_CONN_CLIENTS} open connection${N_CONN_CLIENTS == 1 ? "" : "s"}`);
       }
