0dfddc3023058e087bdfc9531ad9da8eadfa7d38
Markus Voge <sgrubsmyon@users.noreply.github.com>
2025-01-02 00:10:11 +0100

    Also show heat pump load on PV page

--- frontend/h64-quasar/src/pages/PVLive.vue
+++ frontend/h64-quasar/src/pages/PVLive.vue
@@ -1,99 +1,100 @@
 <template>
   <q-page padding>
     <h6>Live photovoltaic values:</h6>
-    <p>Last timestamp: {{ values.faster.time }}</p>
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th>Inverter</th>
+          <th>Heat pump</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>Last timestamp</th>
+          <td>{{ timestampString(inverter_data.values.faster.time) }}</td>
+          <td>{{ timestampString(heatpump_data.values.values2.time) }}</td>
+        </tr>
+        <tr>
+          <th>Status</th>
+          <td>{{ inverter_data.status.type + (inverter_data.status.msg.length > 0 ? ('(' + inverter_data.status.msg + ')') : '') }}</td>
+          <td>{{ heatpump_data.status }}</td>
+        </tr>
+      </tbody>
+    </table>
 
     <div class="row">
-      <value-display label="PV production" :value="`${values.faster.pv1_power + values.faster.pv2_power} W`" />
-      <value-display label="Grid power" :value="`${values.faster.total_grid_power} W`" />
-      <value-display label="Battery power" :value="`${values.faster.battery_power} W`" />
-      <value-display label="Residential load" :value="`${values.faster.total_load_power} W`" />
-      <value-display label="Battery SoC" :value="`${values.slow.battery_soc}%`" />
+      <value-display label="PV production" :value="`${inverter_data.values.faster.pv1_power + inverter_data.values.faster.pv2_power} W`" />
+      <value-display label="Grid power" :value="`${inverter_data.values.faster.total_grid_power} W`" />
+      <value-display label="Battery power" :value="`${inverter_data.values.faster.battery_power} W`" />
+      <value-display label="Total load (house + heat pump)" :value="`${inverter_data.values.faster.total_load_power} W`" />
+      <value-display label="House load" :value="`${Math.round(inverter_data.values.faster.total_load_power - heatpump_data.values.power)} W`" />
+      <value-display label="Heat pump load" :value="`${Math.round(heatpump_data.values.power)} W`" />
+      <value-display label="Battery SoC" :value="`${inverter_data.values.slow.battery_soc}%`" />
     </div>
   </q-page>
 </template>
 
+<style scoped>
+table {
+  border-collapse: collapse;
+  margin-bottom: 10px;
+}
+table th, table td {
+  padding: 5px;
+  border: 1px solid #ccc;
+}
+</style>
+
 <script setup lang="ts">
 import { ref } from 'vue';
 import ValueDisplay from 'components/ValueDisplay.vue';
+import { InverterData, emptyInverterData } from 'components/typesd/inverter-data';
+import { HeatPumpData, emptyHeatPumpData } from 'components/typesd/heat-pump-data';
 
 const RECONNECT_INTERVAL = 10;
 
-const values = ref({
-  slow: {
-    time: '',
-    daily_pv_production: NaN,
-    total_pv_production: NaN,
-    daily_load_energy_consumption: NaN,
-    total_load_energy_consumption: NaN,
-    daily_energy_bought: NaN,
-    daily_energy_sold: NaN,
-    total_energy_bought: NaN,
-    total_energy_sold: NaN,
-    battery_soc: NaN,
-    battery_daily_charge: NaN,
-    battery_daily_discharge: NaN,
-    battery_total_charge: NaN,
-    battery_total_discharge: NaN,
-    dc_temperature: NaN,
-    ac_temperature: NaN,
-    battery_temperature: NaN,
-    battery_voltage: NaN,
-    active_power_regulation: NaN
-  },
-  fast: {
-    time: '',
-    load_voltage_l1: NaN,
-    load_voltage_l2: NaN,
-    load_voltage_l3: NaN,
-    grid_voltage_l1: NaN,
-    grid_voltage_l2: NaN,
-    grid_voltage_l3: NaN,
-    ct_internal_power_l1: NaN,
-    ct_internal_power_l2: NaN,
-    ct_internal_power_l3: NaN,
-    ct_external_power_l1: NaN,
-    ct_external_power_l2: NaN,
-    ct_external_power_l3: NaN,
-    inverter_power_l1: NaN,
-    inverter_power_l2: NaN,
-    inverter_power_l3: NaN,
-    current_l1: NaN,
-    current_l2: NaN,
-    current_l3: NaN,
-    battery_current: NaN
-  },
-  faster: {
-    time: '',
-    total_load_power: NaN,
-    total_grid_power: NaN,
-    load_power_l1: NaN,
-    load_power_l2: NaN,
-    load_power_l3: NaN,
-    pv1_power: NaN,
-    pv2_power: NaN,
-    pv1_voltage: NaN,
-    pv2_voltage: NaN,
-    pv1_current: NaN,
-    pv2_current: NaN,
-    battery_power: NaN
-  }
-});
+const inverter_data = ref<InverterData>(emptyInverterData);
+const heatpump_data = ref<HeatPumpData>(emptyHeatPumpData);
 
-function connectToWebSocketServer() {
+function timestampString(timestamp: string) {
+  const date = new Date(timestamp + 'Z');
+  const date_str = date.toLocaleString('de-DE', {
+    dateStyle: 'medium', timeStyle: 'medium'
+  });
+  return date_str;
+}
+
+function connectToInverterWebSocketServer() {
   let socket = new WebSocket('wss://inverterdata.h64.viridian-project.org');
   socket.onmessage = function (e) {
-    // console.log(JSON.parse(e.data).values);
-    values.value = JSON.parse(e.data).values;
+    // console.log('inverter_data:', JSON.parse(e.data));
+    inverter_data.value = JSON.parse(e.data);
   }
   socket.onclose = function () {
     console.warn('Socket is closed. Reconnect will be attempted in 10 seconds.');
     setTimeout(function () {
-      socket = connectToWebSocketServer();
+      socket = connectToInverterWebSocketServer();
     }, RECONNECT_INTERVAL * 1000);
   }
   return socket;
 }
 
-connectToWebSocketServer();
+function connectToHeatPumpWebSocketServer() {
+  let socket = new WebSocket('wss://heatpumpdata.h64.viridian-project.org');
+  socket.onmessage = function (e) {
+    // console.log('heatpump_data:', JSON.parse(e.data));
+    heatpump_data.value = JSON.parse(e.data);
+  }
+  socket.onclose = function () {
+    console.warn('Socket is closed. Reconnect will be attempted in 10 seconds.');
+    setTimeout(function () {
+      socket = connectToHeatPumpWebSocketServer();
+    }, RECONNECT_INTERVAL * 1000);
+  }
+  return socket;
+}
+
+connectToInverterWebSocketServer();
+connectToHeatPumpWebSocketServer();
 </script>
